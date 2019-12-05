f1c69b37472df34d97ab03e0c671858716076abd
Jane1220 <1521609483@qq.com>
2019-12-05 19:27:37 +0800

    第一次提交

---
+++ src/main/java/com/wangxinyu/demo/Test.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.wangxinyu.demo;
+
+/**
+ * @作者： 王新宇
+ * @时间：2019年12月5日
+ */
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("helloWord");
+		
+	}
+
+}
